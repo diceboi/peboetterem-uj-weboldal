46b724951467c81da85ad422a688d42c46ef592a
diceboi <122260041+diceboi@users.noreply.github.com>
2023-11-29 13:39:23 +0100

    banner

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,6 +3,7 @@
 export const dynamic = "force-dynamic";
 
 import Image from "next/image";
+import HomeBanner from "./HomeBanner";
 
 import { gql } from "@apollo/client"
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
@@ -39,9 +40,9 @@ export default function Hero() {
     <section id="mainhero" className="relative flex flex-col justify-center gap-10 w-full min-h-[90vh] bg-[--navy]">
       <div style={{backgroundImage: `url("${pages.fooldal.heroBackground.sourceUrl}")`}} className="absolute w-full h-full opacity-50 mix-blend-overlay bg-cover bg-no-repeat bg-center"></div>
       <div className="container m-auto flex flex-col gap-10 justify-center items-center z-10">
-        <Image src={pages.fooldal.heroLogo.sourceUrl} alt={pages.fooldal.heroLogo.altText} width={300} height={300} className="max-w-[200px] lg:max-w-[300px] max-h-[200px] lg:max-h-[300px]"/>
+        <Image src={pages.fooldal.heroLogo.sourceUrl} alt={pages.fooldal.heroLogo.altText} width={300} height={300} className="max-w-[150px] lg:max-w-[200px] max-h-[150px] lg:max-h-[200px]"/>
         <p className="w-11/12 lg:w-1/2 text-center text-[--grey]">{pages.fooldal.heroText}</p>
-
+        <HomeBanner classname={"w-11/12 lg:w-full h-[15vh] lg:h-[30vh] bg-slate-50 bg-cover bg-center"}/>
       </div>
 
     </section>
