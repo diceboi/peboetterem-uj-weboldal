b402c0dff1d5d0b9c097571be32a65359af3cdb2
diceboi <122260041+diceboi@users.noreply.github.com>
2023-11-29 13:48:07 +0100

    2nd banner hotfix

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -39,7 +39,7 @@ export default function Hero() {
   return (
     <section id="mainhero" className="relative flex flex-col justify-center gap-10 w-full min-h-[90vh] bg-[--navy]">
       <div style={{backgroundImage: `url("${pages.fooldal.heroBackground.sourceUrl}")`}} className="absolute w-full h-full opacity-50 mix-blend-overlay bg-cover bg-no-repeat bg-center"></div>
-      <div className="container m-auto flex flex-col gap-10 justify-center items-center z-10 pt-[20vh]">
+      <div className="container m-auto flex flex-col gap-10 justify-center items-center z-10 py-[20vh]">
         <Image src={pages.fooldal.heroLogo.sourceUrl} alt={pages.fooldal.heroLogo.altText} width={300} height={300} className="max-w-[150px] lg:max-w-[200px] max-h-[150px] lg:max-h-[200px]"/>
         <p className="w-11/12 lg:w-1/2 text-center text-[--grey]">{pages.fooldal.heroText}</p>
         <HomeBanner classname={"w-11/12 lg:w-10/12 h-[15vh] lg:h-[30vh] bg-slate-50 bg-cover bg-center"}/>
