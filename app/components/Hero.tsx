35a2af7d5b4cdf8479e44c0bc5889056e915eaab
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-21 19:53:34 +0100

    no error

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -10,14 +10,6 @@ import { Toaster, toast } from "sonner";
 
 export default function Hero() {
 
-  useEffect(() => {
-    // Code to execute after component mounts
-    toast.error(`A levelezőrendszerünk átmeneti hibája miatt előfordulhat, hogy rendelés után a visszaigazoló email nem érkezik meg. A hiba javításáig szíves türelmet kérnénk. Köszönjük!`, {
-      className: 'warningtoaster',
-      duration: 10000,
-    });
-  }, []);
-
   return (
     <section id="mainhero" className="relative flex flex-col justify-center items-center gap-10 w-full lg:mt-0 mt-[55px] min-h-[50vh] bg-[--navy]">
       <div className="absolute w-full h-full opacity-50 mix-blend-overlay bg-[url('/herobg-pizza-2.webp')] bg-cover bg-no-repeat bg-center"></div>
@@ -25,7 +17,6 @@ export default function Hero() {
         <Image src='/pebo-hero-logo.png' alt='pebo-logo' width={300} height={300} className="max-w-[100px] lg:max-w-[200px] max-h-[100px] lg:max-h-[200px]"/>
         <p className="w-11/12 lg:w-1/2 text-center lg:text-left text-md lg:text-lg text-[--grey]">Olaszos és klasszikus ízekkel, vagy egy igazán jó kávéval várunk Kaposvár szívében. Szerezz örömteli pillanatokat nálunk hangulatos környezetben.</p>
       </div>
-      <Toaster richColors position="bottom-center"/>
     </section>
   )
 }
