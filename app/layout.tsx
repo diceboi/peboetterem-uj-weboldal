d630cb1d70fa7e1d7006d905e71a0437eaad55ff
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-23 20:20:16 +0100

    no noscript

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,17 +86,7 @@ export default function RootLayout({
       />
 
       </head>
-      <body className={`${bebasNeue.variable} ${inter.variable} ${playball.variable}`}>
-
-      <noscript>
-        <iframe 
-          src="https://www.googletagmanager.com/ns.html?id=GTM-M6QM75MD"
-          height="0" 
-          width="0" 
-          style={{ display: 'none', visibility: 'hidden' }}>
-        </iframe>
-      </noscript>
-      
+      <body className={`${bebasNeue.variable} ${inter.variable} ${playball.variable}`}>      
           <AddToCartProvider>
             <MainNav />
               {children}
