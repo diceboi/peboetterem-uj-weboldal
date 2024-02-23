a3b2222e9d14fb2d01edf163745f5101870daeaf
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-23 18:44:36 +0100

    tagmanager

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import MainNav from './components/Nav/mainnav';
 import Footer from './components/UI/Footer';
 import Script from 'next/script';
 import AddToCartProvider, { AddToCartContext } from './addToCart';
-import { GoogleTagManager } from '@next/third-parties/google'
 
 const bebasNeue = Bebas_Neue({ 
   subsets: ['latin'], 
@@ -38,6 +37,20 @@ export default function RootLayout({
     <html lang="en">
       <head>
 
+      <Script
+        id='tagmanager'
+        strategy='beforeInteractive'
+        dangerouslySetInnerHTML={{
+          __html:`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M6QM75MD');
+          `
+        }}
+      />
+
       <Script 
         id='Hotjar' 
         strategy='beforeInteractive' 
@@ -51,7 +64,8 @@ export default function RootLayout({
           r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
           a.appendChild(r);
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
-      }}>
+        }}
+      />
 
       <Script
         id="fb-pixel"
@@ -69,12 +83,20 @@ export default function RootLayout({
           fbq('init', '1522837111829084');
           fbq('track', 'PageView');`
         }}
-      /></Script>
-  
+      />
+
       </head>
       <body className={`${bebasNeue.variable} ${inter.variable} ${playball.variable}`}>
 
-      <GoogleTagManager gtmId="GTM-M6QM75MD" />
+      <noscript>
+        <iframe 
+          src="https://www.googletagmanager.com/ns.html?id=GTM-M6QM75MD"
+          height="0" 
+          width="0" 
+          style={{ display: 'none', visibility: 'hidden' }}>
+        </iframe>
+      </noscript>
+      
           <AddToCartProvider>
             <MainNav />
               {children}
