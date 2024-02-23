5a043ecfcf19ce66e85fbcee1ea0fc543f6fbd91
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-23 03:26:27 +0100

    experimental try

--- next.config.js
+++ next.config.js
@@ -16,6 +16,9 @@ const nextConfig = {
         },
       ],
     },
+    experimental: {
+      serverComponentsExternalPackages: ['@react-email/render', '@react-email/tailwind']
+    },
     async headers() {
       return [
         {
