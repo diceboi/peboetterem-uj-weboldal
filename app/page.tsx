13bde0287576c209a3e8724080513ebcd0080410
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-23 03:36:15 +0100

    removegallery2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AboutUs from "./components/AboutUs"
 import ContactUs from "./components/ContactUs"
 import Current from "./components/Current"
 import DailyMenu from "./components/DailyMenu"
-import Gallery from "./components/Gallery"
 import Hero from "./components/Hero"
 import Menu from "./components/Menu"
 
