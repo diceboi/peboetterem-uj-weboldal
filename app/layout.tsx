74390db5d9c92017cacd0a09c1ae5ee88077b7d1
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-23 22:08:01 +0100

    script

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,19 +37,18 @@ export default function RootLayout({
     <html lang="en">
       <head>
 
-      <Script
-        id='tagmanager'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html:`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-M6QM75MD');
-          `
-        }}
-      />
+      <script>
+          {`(function(w: Window, d: Document, s: string, l: string, i: string) {
+            w[l] = w[l] || [];
+            w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, 'script', 'dataLayer', 'GTM-M6QM75MD');`}
+      </script>
 
       <Script 
         id='Hotjar' 
