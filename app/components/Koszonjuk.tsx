af52bb7ca8fb42a7275c33fc999898909fb214ff
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-27 22:30:35 +0100

    lets go

--- app/components/Koszonjuk.tsx
+++ app/components/Koszonjuk.tsx
@@ -3,14 +3,10 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { sendGTMEvent } from "@next/third-parties/google";
-import { useSearchParams } from 'next/navigation'
-import Script from 'next/script';
 
 export default function Koszonjuk() {
   const router = useRouter();
   const [countdown, setCountdown] = useState(5);
-  const searchParams = useSearchParams()
-  const value = searchParams.get('value')
 
   useEffect(() => {
 
@@ -33,15 +29,6 @@ export default function Koszonjuk() {
 
   return (
     <>
-    <Script 
-        id='fb-purchase' 
-        strategy='afterInteractive' 
-        dangerouslySetInnerHTML={{
-            __html:
-            `fbq('track', 'Purchase', {value: '${value}', currency: 'HUF'});`
-        }}
-    ></Script>
-
     <section className='flex flex-col items-center justify-center w-full h-[100vh] bg-[--navy]'>
       <h1>Köszönjük a vásárlást, jó étvágyat!</h1>
       <p className='text-[--grey]'>{`${countdown} másodperc múlva visszairányítjuk a főoldalra.`}</p>
--- app/koszonjuk/page.tsx
+++ app/koszonjuk/page.tsx
@@ -1,7 +1,18 @@
+import Script from "next/script";
 import Koszonjuk from "../components/Koszonjuk";
 
-export default function Kapcsolat() {
+export default function Kapcsolat({params, searchParams}:any) {
   return (
+    <>
+    <Script 
+        id='fb-purchase' 
+        strategy='afterInteractive' 
+        dangerouslySetInnerHTML={{
+            __html:
+            `fbq('track', 'Purchase', {value: '${searchParams.value}', currency: 'HUF'});`
+        }}
+    ></Script>
     <Koszonjuk />
+    </>
   )
 }
