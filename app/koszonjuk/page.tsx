8e11ea191276f2a805b03335c30f4256fbe279f5
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-27 22:33:01 +0100

    lets goooo

--- app/koszonjuk/page.tsx
+++ app/koszonjuk/page.tsx
@@ -2,6 +2,7 @@ import Script from "next/script";
 import Koszonjuk from "../components/Koszonjuk";
 
 export default function Kapcsolat({params, searchParams}:any) {
+
   return (
     <>
     <Script 
