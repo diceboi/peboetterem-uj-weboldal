ad22c1261e183afafced6e052d78db443469a15e
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-29 08:11:38 +0100

    no countdown

--- app/components/Koszonjuk.tsx
+++ app/components/Koszonjuk.tsx
@@ -5,37 +5,20 @@ import { useRouter } from 'next/navigation';
 import { sendGTMEvent } from "@next/third-parties/google";
 import { useSearchParams } from 'next/navigation'
 import Script from 'next/script';
+import Link from 'next/link';
 
 export default function Koszonjuk() {
   const router = useRouter();
-  const [countdown, setCountdown] = useState(5);
   const searchParams = useSearchParams()
   const value = searchParams.get('value')
-
-  useEffect(() => {
-
-    sendGTMEvent({ event: 'pageLoaded', value: 'koszonjukPage' });
-
-    const countdownInterval = setInterval(() => {
-      setCountdown((prevCount) => {
-        if (prevCount === 1) {
-          // Redirect to the main page when countdown reaches 1
-          clearInterval(countdownInterval);
-          router.push('/');
-        }
-        return prevCount - 1;
-      });
-    }, 1000);
-
-    // Clear the interval when the component is unmounted
-    return () => clearInterval(countdownInterval);
-  }, [router]);
 
   return (
     <>
     <section className='flex flex-col items-center justify-center w-full h-[100vh] bg-[--navy]'>
       <h1>Köszönjük a vásárlást, jó étvágyat!</h1>
-      <p className='text-[--grey]'>{`${countdown} másodperc múlva visszairányítjuk a főoldalra.`}</p>
+      <Link href='/'>
+        <button className='flex flex-nowrap items-center gap-1 py-1 lg:py-2 px-2 lg:px-4 bg-[--okker] text-[--navy] font-bebas h-min text-sm lg:text-lg rounded-md shadow-md cursor-pointer'>Vissza a főoldalra</button>
+      </Link>
     </section>
     </>
   );
