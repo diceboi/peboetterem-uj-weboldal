3739df53a22b76ce27573c71af6e88defb055ab1
diceboi <122260041+diceboi@users.noreply.github.com>
2024-02-29 17:30:01 +0100

    text-center, padding

--- app/components/Koszonjuk.tsx
+++ app/components/Koszonjuk.tsx
@@ -14,8 +14,8 @@ export default function Koszonjuk() {
 
   return (
     <>
-    <section className='flex flex-col items-center justify-center w-full h-[100vh] bg-[--navy]'>
-      <h1>Köszönjük a vásárlást, jó étvágyat!</h1>
+    <section className='flex flex-col items-center justify-center w-full h-[100vh] bg-[--navy] py-4'>
+      <h1 className='text-center'>Köszönjük a vásárlást, jó étvágyat!</h1>
       <Link href='/'>
         <button className='flex flex-nowrap items-center gap-1 py-1 lg:py-2 px-2 lg:px-4 bg-[--okker] text-[--navy] font-bebas h-min text-sm lg:text-lg rounded-md shadow-md cursor-pointer'>Vissza a főoldalra</button>
       </Link>
