b7246097bb2c5d9d002545cb4481661b4d4c1974
diceboi <122260041+diceboi@users.noreply.github.com>
2024-04-05 14:24:54 +0200

    pebo og

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
+      <meta property="og:image" content="/pebo-og.jpg" />
       <Script 
         id='Hotjar' 
         strategy='beforeInteractive' 
