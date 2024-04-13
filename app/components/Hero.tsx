3eb1c573bc9248838ca906d66561583a164a8212
diceboi <122260041+diceboi@users.noreply.github.com>
2024-04-13 11:23:44 +0200

    hero correction

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,9 +12,9 @@ import Link from "next/link";
 export default function Hero() {
 
   return (
-    <section id="mainhero" className="relative flex flex-col justify-center items-center gap-10 w-full lg:mt-0 mt-[55px] min-h-[60vh] bg-[--navyblur]">
+    <section id="mainhero" className="relative flex flex-col justify-center items-center gap-10 w-full lg:mt-0 mt-[55px] lg:min-h-[60vh] min-h-[80vh] bg-[--navyblur]">
       <div className="absolute w-full h-full opacity-100 mix-blend-overlay bg-[url('/herobg.webp')] bg-cover bg-no-repeat bg-center"></div>
-      <div className="container flex flex-wrap lg:flex-nowrap gap-10 justify-center items-center z-10 max-w-[1024px]">
+      <div className="container flex flex-wrap lg:flex-nowrap gap-10 justify-center items-center z-10 max-w-[1024px] lg:mb-0 mb-20">
         <Image src='/pebo-hero-logo.png' alt='pebo-logo' width={300} height={300} className="max-w-[100px] lg:max-w-[200px] max-h-[100px] lg:max-h-[200px]"/>
         <div className="flex flex-col gap-4">
           <p className="w-full text-center lg:text-left text-md lg:text-lg text-white font-medium">Olaszos és klasszikus ízekkel, vagy egy igazán jó kávéval várunk Kaposvár szívében. Szerezz örömteli pillanatokat nálunk hangulatos környezetben.</p>
