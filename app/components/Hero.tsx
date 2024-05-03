a0c12f4c7b3a2bd1889c49f57afe11a254bc2962
diceboi <122260041+diceboi@users.noreply.github.com>
2024-05-03 20:28:23 +0200

    close 3

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,8 +14,8 @@ export default function Hero() {
   return (
     <section id="mainhero" className="relative flex flex-col justify-center items-center gap-10 w-full lg:mt-0 mt-[55px] lg:min-h-[60vh] min-h-[80vh] bg-[--navyblur]">
       <div className="absolute w-full h-full opacity-100 mix-blend-overlay bg-[url('/herobg.webp')] bg-cover bg-no-repeat bg-center"></div>
-      <div className="container flex flex-col lg:flex-nowrap gap-10 justify-center items-center z-10 w-1/3 lg:mb-0 mb-20 bg-[--navyblur] p-4 backdrop-blur-sm border border-[--lightnavy] rounded-lg">
-        <h1>Kedves vendégeink</h1>
+      <div className="container flex flex-col lg:flex-nowrap gap-10 justify-center items-center z-10 lg:w-1/3 w-11/12 lg:mb-0 mb-20 bg-[--navyblur] p-4 backdrop-blur-sm border border-[--lightnavy] rounded-lg">
+        <h1 className="text-center">Kedves vendégeink</h1>
         <p className="text-center text-white">A nyári időszakra éttermünk a mai napon (2024.05.03.) bezár, megértéseteket köszönjük! Minden jót kíván a PEBo Étterem teljes csapata!</p>
         
       </div>
